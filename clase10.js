af0d31fadc9f80da0ead03cadac2294bea4de640
Jose Luis Acevedo <105613336+Joseacevedoo@users.noreply.github.com>
2023-07-13 18:56:59 -0300

    Add files via upload

---
+++ clase10.js
@@ -0,0 +1,15 @@
+let a,b,c;
+
+a= parseInt(prompt('Introduzca el primer numero (entero):'));
+b =parseInt(prompt('Ingresar el segundo numero (entero):'));
+c = a + b;
+document.write('La suma es '+c);
+
+// *Para HTML*
+//<script>let a,b,c;
+
+//a= parseInt(prompt('Introduzca el primer numero (entero):'));
+//b =parseInt(prompt('Ingresar el segundo numero (entero):'));
+//c = a + b;
+//document.write('La suma es '+c);
+//</script> 
